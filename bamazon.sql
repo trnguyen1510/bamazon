fa9148a4dfaa0e1e23d155ae1ee13df27d0696f3
TNguyen <trnguyen1510@gmail.com>
2019-03-10 14:53:22 -0700

    removed passcode

--- bamazon.sql
+++ bamazon.sql
@@ -4,8 +4,8 @@ USE Bamazon;
 
 CREATE TABLE products (
 	item_id INTEGER(11) AUTO_INCREMENT NOT NULL,
-	product_name VARCHAR(30) NOT NULL,
-	department_name VARCHAR(20) NOT NULL,
+	product_name VARCHAR(60) NOT NULL,
+	department_name VARCHAR(60) NOT NULL,
 	price DECIMAL(10,2) NOT NULL,
 	stock_quantity INTEGER(11) NOT NULL,
 	PRIMARY KEY (item_id)
